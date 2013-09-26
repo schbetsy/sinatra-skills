6b04afef9a8347efcd9a713d73419fc4486c39eb
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-26 10:18:06 -0500

    added validations, will uncomment later

--- app/models/proficiency.rb
+++ app/models/proficiency.rb
@@ -1,4 +1,9 @@
 class Proficiency < ActiveRecord::Base
   belongs_to :user
   belongs_to :skill
+
+  # validates :user, presence: true
+  # validates :skill, presence: true
+  # validates :experience, numericality: {only_integer: true}
+  # validates :formal_education, inclusion: {in: [true, false]}
 end
