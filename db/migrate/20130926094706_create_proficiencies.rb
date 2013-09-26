fba0f4c72acdc90ace7aa410e0e551a3dcbc550e
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-26 10:06:15 -0500

    association of skills to users through proficiencies

--- db/migrate/20130926094706_create_proficiencies.rb
+++ db/migrate/20130926094706_create_proficiencies.rb
@@ -1,4 +1,12 @@
 class CreateProficiencies < ActiveRecord::Migration
   def change
+    create_table :proficiencies do |t|
+      t.references :user
+      t.references :skill
+      t.integer :experience
+      t.boolean :formal_education
+
+      t.timestamps
+    end
   end
 end
